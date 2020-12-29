8a6779173803ade2ad3e8720edb857e45f416b77
JalinWu <lily.wu035@gmail.com>
2020-12-29 22:21:09 +0800

    dev

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ app.get('/', (req, res) => {
     res.send('HelloWorld!');
 })
 
-app.listen(3000, () => {
+app.listen(3003, () => {
     console.log('[BOT已準備就緒]');
-    console.log('Server started on port 3000');
+    console.log('Server started on port 3003');
 });
