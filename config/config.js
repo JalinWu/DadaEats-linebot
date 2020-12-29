fad4d81bf0f6415b93bb57fc92eb703c40ef3799
JalinWu <lily.wu035@gmail.com>
2020-12-29 22:14:24 +0800

    dev

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const channelData = {
+    channelId: '1654992668', 
+    channelSecret: 'd1d6cd01e11b483717233c15677804a8', 
+    channelAccessToken: '5vBr57ouitRfuENZ5HneA4wlwmsx5EP6mQquZ4c84iGqMCOrYzjWP3BpHd8R5QtoYQD6Gkffq6qbt6+dQPbkWadC29FcH/9V61P4c/F2MnzT/2/31RelxydEtk/GVhUkhN/ZuSGG89EHs6zqzX4VEwdB04t89/1O/w1cDnyilFU='
+}
+
+module.exports = {
+    channelData
+};
